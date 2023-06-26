5f8b7f1f7aa6422e37d404154d1a5963fdab07fd
ThamizhselviSelvaraj <thamizhselvis@scrunchworks.com>
2023-06-26 13:57:51 +0530

    shop application

---
+++ Finance/src/test/java/com/pro/Finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.Finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
